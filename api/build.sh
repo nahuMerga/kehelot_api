2b4ac86326a45810612bbca4557ab2bc7b6629f8
NahoMer <143824687+nahuMerga@users.noreply.github.com>
2025-02-14 16:08:36 -0300

    Update build.sh

--- api/build.sh
+++ api/build.sh
@@ -4,6 +4,8 @@ set -o errexit
 
 pip install --upgrade pip
 pip install -r requirements.txt 
-python manage.py runserver
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py runserver
+
+
