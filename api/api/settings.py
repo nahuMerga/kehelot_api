162676287d9393c24f5eebf4a8df1b14a79a1741
NahoMer <143824687+nahuMerga@users.noreply.github.com>
2025-02-14 10:20:45 -0300

    Update settings.py

--- api/api/settings.py
+++ api/api/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t#8f&57c$l9h!t(h=@(&*)$sj+&r9x@%_n9p4+04ins!((2nc2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["kehelot-backend.onrender.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
