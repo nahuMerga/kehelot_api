a16fe9cc444dd631d8b9baecad555dbb1f923bf0
NahoMer <143824687+nahuMerga@users.noreply.github.com>
2025-02-15 07:37:21 -0300

    Update urls.py

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('merga_123334/', admin.site.urls),
     path('kehelot_ai/', include('kehelot_ai.urls')), 
 ]
+
+urlpatterns += staticfiles_urlpatterns()
