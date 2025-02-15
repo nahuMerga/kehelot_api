5daa3637b60162a2756b09ae7c315cb73a640179
NahoMer <143824687+nahuMerga@users.noreply.github.com>
2025-02-15 06:01:34 -0300

    Update settings.py

--- api/api/settings.py
+++ api/api/settings.py
@@ -104,7 +104,6 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD', 'default_password'), 
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
-        'CONN_MAX_AGE': 600,
     }
 }
 
