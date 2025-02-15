8cb394b9641b5f4d733e033231ce85fc8ee5678a
NahoMer <143824687+nahuMerga@users.noreply.github.com>
2025-02-15 08:57:36 -0300

    Update settings.py

--- api/api/settings.py
+++ api/api/settings.py
@@ -156,7 +156,7 @@ SIMPLE_JWT = {
 STATIC_URL = '/static/' 
 
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
